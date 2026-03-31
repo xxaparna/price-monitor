6bc01b4cab2a1a50be48788009cb8ab6b7409ac8
xxaparna <156069499+xxaparna@users.noreply.github.com>
2026-03-31 18:12:53 +0530

    last commit

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 export default function Navbar() {
   return (
     <nav>
-      <Link to="/" className="brand">📈 Price Monitor</Link>
+      <Link to="/" className="brand"> Price Monitor</Link>
       <Link to="/">Dashboard</Link>
       <Link to="/products">Products</Link>
     </nav>
